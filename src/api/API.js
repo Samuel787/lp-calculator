bef54ab7fbc4d46dc1d859d2be188c21682fe1ff
Adithya Narayan <adithya.narayan@shopee.com>
2021-05-13 17:55:40 +0800

    Add getGasFees

---
+++ src/api/API.js
@@ -0,0 +1,25 @@
+import axios from "axios"
+
+// returns estimated gas fees for Uniswap LP add in ETH
+export async function getGasFees(speed = "fast") {
+
+    const response = await axios.get("https://www.etherchain.org/api/gasPriceOracle")
+
+    // estimated gas usage of Uniswap v3 contract
+    // for LP add
+    // Source: https://bit.ly/3uPsdNq (Reddit Post)
+    const gasUsage = 507000
+
+    const gweiToEth = 0.000000001
+    
+    switch (speed) {
+        case "safeLow":
+        case "standard":
+        case "fast":
+        case "fastest":
+            return response.data[speed] * gasUsage * gweiToEth
+        default:
+            return response.data.fastest * gasUsage * gweiToEth
+    }
+
+}
